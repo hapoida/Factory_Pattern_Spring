76583fb2bacc92235dc7ff788477cdad45e7d790
hapoida <hapoida@hanmail.net>
2014-07-17 17:34:58 +0900

    불필요 Interface 제거

--- Factory_Pattern_Spring/Factory_Pattern_Spring/src/main/java/kr/co/hapoida/factory/Factory.java
+++
@@ -1,9 +0,0 @@
-package kr.co.hapoida.factory;
-
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.context.annotation.Bean;
-
-public interface Factory {
-	
-	public TestType1 create();
-}
